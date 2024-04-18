bc69b36d51bc182b7b06b8719db8b4358406edcb
Mijan3j <133229714+Mijan3j@users.noreply.github.com>
2024-04-18 17:36:02 +0600

    Update 0_MR-PREM-BABU.js

--- modules/commands/0_MR-PREM-BABU.js
+++ modules/commands/0_MR-PREM-BABU.js
@@ -1,32 +1,23 @@
+const fs = require("fs");
 module.exports.config = {
-  name: "prem",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "PREM BABU",
-  description: "THIS BOT IS MADE BY PREM BABU",
-  commandCategory: "PREM BABU IMAGE",
-  usages: "PREM-BABU",
-  cooldowns: 2,
-  dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
-    
-};
-
-module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-    var link = [
-"https://i.imgur.com/bJkwri4.jpeg",
-"https://i.imgur.com/Rd84Bhe.jpeg",
-"https://i.imgur.com/71hdujn.jpeg",
-"https://i.imgur.com/0sWGhwe.jpeg",
-"https://i.imgur.com/86kmX7S.jpeg",
-"https://i.imgur.com/jrYsBnF.jpeg"
-     ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     प्रेम-बाबू                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे बॉस प्रेम बाबू को सबसे अलग मासूम चेहरा भोली सी सूरत आखों में प्यार दिल में खुमार ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
-   };
+	name: "😏",
+	    version: "1.0.1",
+	    	hasPermssion: 0,
+	    		credits: "VanHung - Fixed by LTD", 
+	    			description: "hihihihi",
+	    				commandCategory: "no prefix",
+	    					usages: "Tutul",
+	    					    cooldowns: 5, 
+	    					    };
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+		if (event.body.indexOf("😼")==0 || event.body.indexOf("😏")==0 || event.body.indexOf("😎")==0 || event.body.indexOf("😏")==0) {
+				var msg = {
+								body: "বাবারে বাবা কি মুখ মচুরায় মুক্ত ভাইঙ্গা যাইবো না ভাঙলে আমি উষ্টা মাইরা ভাইঙ্গা ফালামু-!!😹🤣",
+								}
+											api.sendMessage( msg, threadID, messageID);
+											    api.setMessageReaction("🤣", event.messageID, (err) => {}, true)
+											    		}
+											    			}
+											    				module.exports.run = function({ api, event, client, __GLOBAL }) {
+  
